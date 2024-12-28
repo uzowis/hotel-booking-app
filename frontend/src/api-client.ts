00d6bd4e5d84bcd397e558f6dc1c05bb88719323
WISDOM ATURUOBI <Uzochukwu Aturuobi>
2024-12-28 16:21:34 +0000

    BEFORE DEPLOYING TO RENDER

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -8,7 +8,8 @@ import {
   SearchParams,
 } from "../../mern-booking-app/backend/src/shared/types";
 import { BookingFormData } from "./forms/BookingForm/BookingForm";
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8001";
 
 export const fetchCurrentUser = async (): Promise<UserType> => {
   const response = await fetch(`${API_BASE_URL}/api/users/me`, {
